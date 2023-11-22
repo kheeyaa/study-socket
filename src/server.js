182007cd98cf1a10001077a6d44a8ddb1320a0ba
cherry <kakao@kakaoui-MacBookPro-73.local>
2023-11-22 11:19:48 +0900

    :sparkles: Send WebSoket Messages

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
-import express from 'express';
+import http from 'http'
+import WebSocket from 'ws'
+import express from 'express'
+const app = express()
 
-const app = express();
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+app.use('/public', express.static(__dirname + '/public'))
+app.get('/', (_, res) => res.render('home'))
+app.get('/*', (_, res) => res.redirect('/'))
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+wss.on('connection', (socket) => {
+  console.log('Connected to Browser ✅')
+  socket.send('hello')
+  socket.on('close', () => console.log('Disconnected from the Browser ❌'))
+  socket.on('message', (message) => console.log(message.toString('utf8')))
+})
+
+const handleListen = () => console.log('Listening on http://localhost:3000')
+server.listen(3000, handleListen)
